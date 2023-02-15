e3412bbedeacf3774d479a9020b33936ee10257e
jingwen-the-creator <zhu-jingwen@qq.com>
2023-02-15 23:21:53 +0800

    static resources added

---
+++ store-static-oss/src/main/java/com/example/statics/StaticOssApplication.java
@@ -0,0 +1,13 @@
+package com.example.statics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticOssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StaticOssApplication.class,args);
+    }
+
+}
